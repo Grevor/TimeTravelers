1e0d4f911dd6cc190f67e1494b6e1ef4e1d07226
Andreas <andrun92@gmail.com>
2013-08-27 21:59:44 +0200

    Changed so that a GameObject not extends Terrain. A GameObject is atm a Renderable so all GameObjects atm needs to have textures.

--- Time Travelers/src/time/travelers/core/GameObject.java
+++ Time Travelers/src/time/travelers/core/GameObject.java
@@ -1,11 +1,12 @@
 package time.travelers.core;
 
+import time.travelers.graphics.Renderable;
 import time.travelers.graphics.TextureObject;
 
-public class GameObject extends Terrain {
+public class GameObject extends Renderable {
 
-	public GameObject(TextureObject tex, Traversal t, double traversalCost) {
-		super(tex, t, traversalCost);
+	public GameObject(TextureObject tex, double x, double y, double z, double w, double h, boolean visiblety) {
+		super(tex, x, y, z, w, h, visiblety);
 		// TODO Auto-generated constructor stub
 	}
 
